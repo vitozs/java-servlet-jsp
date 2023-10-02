222d1ea8bf6e647d949768a0d80a2849e01cd2af
vitozs <vitornuneschagas2016@gmail.com>
2023-10-02 19:02:23 -0300

    First commit

---
+++ src/main/java/com/br/servlets/Servlet.java
@@ -0,0 +1,47 @@
+package com.br.servlets;
+
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.ServletRequest;
+import jakarta.servlet.ServletResponse;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+
+
+@WebServlet(name = "Servlet", urlPatterns = "/servlet")
+public class Servlet extends HttpServlet {
+
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+
+        //encaminhar a requisição atual para outro recurso.
+        RequestDispatcher requestDispatcher = request.getRequestDispatcher("pg2.jsp");
+
+        //Pega o parametro "texto", que foi passado no forms do index.html
+        String texto = request.getParameter("texto");
+
+        //Seta o nome do atributo que vai ser resgatado na pg .jsp e o valor dele
+        request.setAttribute("texto", texto);
+
+
+        //Passa adiante o request e a response para o recurso, no caso pg2.jsp
+        requestDispatcher.forward(request, response);
+
+
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        processRequest(req, resp);
+
+    }
+}
---
+++ src/main/webapp/pg2.jsp
@@ -0,0 +1,8 @@
+<html>
+<body>
+<h2>Hello World!</h2>
+<p>Pagina JSP</p>
+<p>Texto digitado na pagina anterior: </p>
+<%= request.getAttribute("texto") %>
+</body>
+</html>
